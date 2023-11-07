e3c58e4964e66ec1e675e974ab9beccbb6f370ee
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-11-07 00:41:38 +0000

    abstract fonts

---
+++ app/fonts.ts
@@ -0,0 +1,14 @@
+import { Poppins, Anonymous_Pro } from 'next/font/google';
+
+export const poppins = Poppins({
+    weight: ['400', '600'],
+    subsets: ['latin'],
+  })
+  
+
+export const anonymousPro = Anonymous_Pro({
+    weight: ['400'],
+    subsets: ['latin'],
+  })
+
+
