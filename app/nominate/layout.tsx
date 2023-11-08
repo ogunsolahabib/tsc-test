d78e2c1d6629ff4ad906879046102e49ee4bad24
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-11-08 04:06:02 +0000

    table component

--- app/nominate/layout.tsx
+++ app/nominate/layout.tsx
@@ -18,7 +18,7 @@ export default function Layout({
 }) {
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-8">
+        <main className="flex min-h-screen px-8 py-20">
             <Container>
                 {children}
             </Container>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import NominateReasoning from './components/pages/NominateReasoning'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+    <main className="flex min-h-screen px-8 py-20">
       <NominateReasoning />
     </main>
   )
