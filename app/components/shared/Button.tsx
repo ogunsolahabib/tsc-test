2efb4f8d9911fc2a7fdd1b0c2be2e16dd1b04410
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-11-09 08:30:22 +0000

    button classes

--- app/components/shared/Button.tsx
+++ app/components/shared/Button.tsx
@@ -12,7 +12,7 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps & React.HTMLProps<HTMLButtonElement>> = ({ variant, children, className, type, loading, width = 'large', ...props }) => {
-    const classes = classNames('px-4 py-2  w-full md:w-auto font-semibold text-sm transition-background duration-300 uppercase disabled:cursor-not-allowed',
+    const classes = classNames('px-4 py-2  font-semibold text-sm transition-background duration-300 uppercase disabled:cursor-not-allowed',
         {
             "bg-black text-white hover:bg-white hover:text-black hover:border-2 hover:border-black disabled:bg-tsc-mid-grey disabled:hover:border-0 disabled:hover:text-white": variant === "primary",
             "bg-white text-black border-2 border-black hover:bg-black hover:text-white disabled:border-gray-300 disabled:text-gray-400 disabled:text-gray-500": variant === "secondary",
@@ -20,6 +20,8 @@ const Button: React.FC<ButtonProps & React.HTMLProps<HTMLButtonElement>> = ({ va
             'min-w-[12rem] md:min-w-[18rem]': width === "large",
             'min-w-[8rem] min-w-[12rem]': width === "medium",
             'md:min-w-[8rem]': width === "small",
+            'w-full': !width || className?.includes('block'),
+            'w-fit': !width || className?.includes('inline'),
         }, className
     )
 
