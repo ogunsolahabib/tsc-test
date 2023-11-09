f87035883eb207d34084412e080e5f57f89ac70a
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-11-09 10:39:25 +0000

    utilities for network requests

---
+++ app/actions.ts
@@ -0,0 +1,45 @@
+'use server';
+
+import { cookies } from "next/headers";
+import { API_BASE_URL } from "./config";
+import { redirect } from "next/navigation";
+
+export async function loginUser(formData: FormData) {
+    const cookieStore = cookies();
+
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    try {
+        const res = await fetch(`${API_BASE_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+
+        const data = await res.json();
+
+
+        if (data?.data?.authToken) {
+
+            cookieStore.set('tsc-authToken', decodeURI(data.data.authToken), {
+                httpOnly: true,
+                maxAge: 60 * 60 * 24 * 30,
+                sameSite: 'lax',
+                path: '/'
+            })
+            redirect('/');
+        }
+
+
+
+    } catch (err) {
+        console.log(err)
+    }
+
+}
--- app/hooks/useFetchRequest.ts
+++ app/hooks/useFetchRequest.ts
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { API_BASE_URL } from "../config";
 import useLocalStorage from "./useLocalStorage";
 
-export default function useGetRequest(pathname: string) {
+export default function useFetchRequest(pathname: string) {
 
     const [data, setData] = useState<any>();
 
     const { storedValue: authToken} = useLocalStorage('tsc-authToken', 0);
 
-    const fetchData = async () => {
+    const GET = async () => {
         const response = await fetch(`${API_BASE_URL}/${pathname}`, {
             method: 'GET',
             headers: {
@@ -20,7 +20,20 @@ export default function useGetRequest(pathname: string) {
         setData(json);
     };
 
-    return { data, fetchData };
+    const POST = async (data: any) => {
+        const response = await fetch(`${API_BASE_URL}/${pathname}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `Bearer ${authToken}`
+            },
+            body: JSON.stringify(data)
+        });
+        const json = await response.json();
+        setData(json);
+    }
+
+    return { data, GET, POST };
 
 
 }
