1bbfdf5f89399b44ed523abf6152a35ba299db03
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-11-09 10:39:45 +0000

    create route paths

---
+++ app/utils/routePaths.ts
@@ -0,0 +1,11 @@
+const routePaths = {
+    login: '/login',
+    nominate: '/nominate',
+    start: '/nominate/enter-nominee',
+    overview: 'nominate/overview',
+    reason: '/nominate/reason',
+    rating: '/nominate/rating',
+    success: '/success',
+}
+
+export default routePaths;
