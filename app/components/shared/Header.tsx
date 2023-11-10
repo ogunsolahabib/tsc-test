dabae33dd0973d3cc7265cedda74d73cf1fe5890
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-11-10 02:59:09 +0000

    sticky header

--- app/components/shared/Header.tsx
+++ app/components/shared/Header.tsx
@@ -12,7 +12,7 @@ export default async function Header() {
     const nominationsRes = await import('@/app/api/nominations/route');
     const nominationsData = await (await nominationsRes.GET()).json();
 
-    return <header className="w-full p-5 flex items-center justify-between bg-black text-white">
+    return <header className="w-full p-5 flex items-center justify-between bg-black text-white sticky top-0 z-50">
         <a href="/">
             <LogoSvg />
         </a>
@@ -22,7 +22,9 @@ export default async function Header() {
             </a>
 
             <a href={routePaths.nominations}><span className="hidden md:block">Your Nominations ({nominationsData.length})</span>
-                <span className="md:hidden"><Inbox /></span>
+                <span className="md:hidden">
+                    <Inbox />
+                </span>
 
             </a>
         </div>
