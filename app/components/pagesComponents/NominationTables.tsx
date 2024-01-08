dbe27da036d307c1db3b6cd8c4914d668f6a4f88
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-01-08 04:11:37 +0000

    rename folder to pagesComponents

--- app/components/pagesComponents/NominationTables.tsx
+++ app/components/pagesComponents/NominationTables.tsx
@@ -15,6 +15,7 @@ import TableEmptyState from "./TableEmptyState";
 import { Nomination } from "@/app/types/Nomination";
 import TableMobile from "./TableMobile";
 import NominationActionButtons from "./NominationActionButtons";
+import ProtectedComponent from "../shared/ProtectedComponent";
 
 
 
@@ -75,7 +76,7 @@ export default function NominationTables({ data }: { data: Nomination[] }) {
 
 
 
-    return <>{data.length === 0 ? <TableEmptyState /> : <>
+    return <ProtectedComponent>{data.length === 0 ? <TableEmptyState /> : <>
         <Container className='md:max-w-full md:w-[76rem] px-0 !bg-transparent'>
             <h1 className="text-3xl font-bold uppercase mb-6">Your nominations</h1>
             <div className={"space-x-3 mb-4"}>
@@ -92,7 +93,7 @@ export default function NominationTables({ data }: { data: Nomination[] }) {
             {showDeleteModal.nomination_id && <ConfirmDeleteNominationModal isOpen={showDeleteModal.visible} nomination_id={showDeleteModal.nomination_id} onClose={() => setShowDeleteModal({ visible: false, nomination_id: null })} />}
         </Container>
     </>
-    }</>
+    }</ProtectedComponent>
 
 
 
